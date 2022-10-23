372d32be94db8334d031dc29bfabc970f378fbec
Micheala Palmer <michealapalmer@gmail.com>
2022-10-23 00:17:05 -0400

    setup backend using node and MongoDB Azure

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://socialmedia:R2r034G8EYsmkhR6@cluster0.diwmcil.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => app.listen(PORT, ()=> console.log(`Server running at ${PORT}`)))
+.catch((error) => console.log(error.message));
