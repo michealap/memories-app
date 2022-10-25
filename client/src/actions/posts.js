4d635eb09e66d0063cc1d8680dc0992c72e82871
Micheala Palmer <michealapalmer@gmail.com>
2022-10-25 08:23:49 -0400

    add constants for action types

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,4 +1,5 @@
 import * as api from '../api';
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
 
 /**  Notes 
  * Action creators - function that return actions
@@ -17,7 +18,7 @@ export const getPosts = () => async(dispatch)=> {
   try {
     const { data } = await api.fetchPosts();
 
-    dispatch({ type: 'FETCH_ALL', payload: data })
+    dispatch({ type: FETCH_ALL, payload: data })
 
   } catch(error) {
     console.log(error.message);
@@ -29,7 +30,7 @@ export const createPost = (post) => async(dispatch)=> {
   try {
     const { data } = await api.createPost(post);
 
-    dispatch({ type: 'CREATE', payload: data })
+    dispatch({ type: CREATE, payload: data })
 
   } catch(error) {
     console.log(error.message);
@@ -41,7 +42,7 @@ export const updatePost = (id, post) => async(dispatch)=> {
   try {
     const { data } = await api.updatePost(id, post);
 
-    dispatch({ type: 'UPDATE', payload: data })
+    dispatch({ type: UPDATE, payload: data })
 
   } catch(error) {
     console.log(error.message);
@@ -53,7 +54,7 @@ export const likePost = (id) => async (dispatch) => {
   try {
     const { data } = await api.likePost(id);
 
-    dispatch({ type: 'LIKE', payload: data });
+    dispatch({ type: LIKE, payload: data });
   } catch (error) {
     console.log(error.message);
   }
@@ -63,7 +64,7 @@ export const deletePost = (id) => async (dispatch) => {
   try {
     await api.deletePost(id);
 
-    dispatch({ type: 'DELETE', payload: id });
+    dispatch({ type: DELETE, payload: id });
   } catch (error) {
     console.log(error.message);
   }
